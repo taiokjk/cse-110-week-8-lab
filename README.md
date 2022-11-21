8958c0dd8b04dd7b060801d2f7cb6c3794e478ac
Liam <lnguyen@ucsd.edu>
2022-11-20 17:07:41 -0800

    Finished part 1 and 2

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Lab 8 - Starter
+Question 1: Manually run them locally before pushing code  
+The reason being that we should complete our automated test at the development level and make sure and tests passed and have a working build before pushing code to the repository.  
+
+Question 2: Would you use an end to end test to check if a function is returning the correct output?  
+No  
+
+Question 3: Would you use a unit test to test the “message” feature of a messaging application? Why or why not?  
+I would NOT use a unit test because the "message" feature usually requires a lot of highly coupled functions to it.  
+
+Question 4: Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not?  
+I would use a unit test because max message length is a simple individual check.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,67 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('valid phone number', () => {
+    expect(functions.isPhoneNumber('510-766-8006')).toBe(true);
+});
+test('valid phone number', () => {
+    expect(functions.isPhoneNumber('1-800-381-8042')).toBe(true);
+});
+test('invalid phone number', () => {
+    expect(functions.isPhoneNumber('hello-world')).toBe(false);
+});
+test('invalid phone number', () => {
+    expect(functions.isPhoneNumber('123-567-890')).toBe(false);
+});
+
+test('valid email', () => {
+    expect(functions.isEmail('lnguyen@ucsd.edu')).toBe(true);
+});
+test('valid email', () => {
+    expect(functions.isEmail('anothervalid@gmail.com')).toBe(true);
+});
+test('invalid email', () => {
+    expect(functions.isEmail('nonvalid@gmail')).toBe(false);
+});
+test('invalid email', () => {
+    expect(functions.isEmail('wrongemail')).toBe(false);
+});
+
+test('valid password', () => {
+    expect(functions.isStrongPassword('letterpassword')).toBe(true);
+});
+test('valid password', () => {
+    expect(functions.isStrongPassword('pass1_hello')).toBe(true);
+});
+test('invalid password', () => {
+    expect(functions.isStrongPassword('123wrong')).toBe(false);
+});
+test('invalid password', () => {
+    expect(functions.isStrongPassword('wrongbecauseitistoolongofapassword')).toBe(false);
+});
+
+test('valid date', () => {
+    expect(functions.isDate('1/1/1970')).toBe(true);
+});
+test('valid date', () => {
+    expect(functions.isDate('05/06/2022')).toBe(true);
+});
+test('invalid date', () => {
+    expect(functions.isDate('01-10-2021')).toBe(false);
+});
+test('invalid date', () => {
+    expect(functions.isDate('1992/06/20')).toBe(false);
+});
+
+test('valid hex color', () => {
+    expect(functions.isHexColor('#92c')).toBe(true);
+});
+test('valid hex color', () => {
+    expect(functions.isHexColor('#e7bba9')).toBe(true);
+});
+test('invalid hex color', () => {
+    expect(functions.isHexColor('#ABCDEFG')).toBe(false);
+});
+test('invalid hex color', () => {
+    expect(functions.isHexColor('#veryWrong')).toBe(false);
+});
